caacc637712d8a94bca9e9b285a0b688a212c885
Joseph Rhines <jprhines@gmail.com>
2021-02-05 09:57:24 -0600

    I deleted the line that included the console.table  because I will use the built in version

--- EmployeeTrackerDBConnection.js
+++ EmployeeTrackerDBConnection.js
@@ -1,13 +1,13 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var consoleTable = require("");
+
 
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'root',
-    database: 'employee_trackerDB',
+    database: 'employee_trackDB',
 });
 
 connection.connect((err) => {
