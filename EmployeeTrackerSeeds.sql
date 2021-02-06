1353c9b90acb23435f4079b3087560e1bb6576c6
Joseph Rhines <jprhines@gmail.com>
2021-02-05 21:36:15 -0600

    I added values to all three tables

--- EmployeeTrackerSeeds.sql
+++ EmployeeTrackerSeeds.sql
@@ -29,4 +29,26 @@ select * from department;
 select * from roles;
 select * from employee;
 
+INSERT INTO department (name)
+VALUES ("HR"), ("Operations"), ("Mail"), ("R&D");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Receptionist", 27000, 1), ("Clerk", 25000, 2), ("Engineer", 46000, 3), ("Designer", 50000, 4 ), ("Manager", 100000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Maria", "Lopez", 3, 7);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Rob", "Smith", 1);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("John", "Harper", 2);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Bob", "Zane", 4);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Jenny", "Vang", 5);
+
+
 
